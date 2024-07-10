7d5b8ceb2944af645fcaacd578eb281e65bae50d
Adam Prentice <adam@adamprentice.co.uk>
2024-07-10 21:22:06 +0100

    Getting rid of template stuff and creating resume and mentoring routes

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+import "./page.scss";
+
+export const metadata: Metadata = {
+  title: "Resume - Adam Prentice",
+  description: "My Resume",
+};
+
+export default function Home() {
+  return <main className=""></main>;
+}
